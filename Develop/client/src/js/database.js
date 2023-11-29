21ce490d8efb6cc8bc69c009ccc399156b9b90a8
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-11-28 16:41:52 -0800

    completed all todos in client folder

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT into db');
+  const jateDb = await openDB('JATE', 1);
+  const tx = jateDb.transaction('JATE', 'readwrite');
+  const store = tx.objectStore('JATE');
+  const request = store.put({id: 1, content:content});
+  const result = await request;
+  return result;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (id) => {
+console.log('GET from db');
+const jateDb = await openDB('JATE', 1)
+const tx = jateDb.transaction('JATE', 'readonly');
+const store = tx.objectStore('JATE');
+const request = store.get(id);
+const result = await request;
+return result;
+};
 
 initdb();
